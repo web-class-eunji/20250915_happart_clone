36382a14d7b97ef1324ab6364a9d38ff7d83ba99
eunji <moeunji12@gmail.com>
2025-10-30 12:48:53 +0900

    dd

--- script/script.js
+++ script/script.js
@@ -5,9 +5,39 @@ let posY = 0;
 
 window.addEventListener("scroll",(e)=>{
     if(posY < window.scrollY){
-        // 헤더숨기기
+        headerHide()
     }else{
-        // 헤더보이기
+        headershow()
     }
     posY = window.scrollY;
 });
+
+/* 
+헤더 숨기는 함수 만들어서 header에 hide라는 클래스 붙이기
+헤더 보이는 함수 만들어서 header에 hide라는 클래스 떼기
+*/
+
+function headerHide(){
+    header.classList.add("hide");
+}
+
+function headershow(){
+    header.classList.remove("hide");
+}
+
+// 스크롤페이드인인터랙션
+const content = document.getElementsByClassName("content");
+console.log(content);
+window.addEventListener("scroll",()=>{
+    const winH = window.innerHeight;
+    for(let i = 0; i < content.length; i++){
+        const contentTop = content[i].getBoundingClientRect().top;
+        if(contentTop - winH < 0){
+            content[i].classList.add("in");
+        }else {
+            content[i].classList.remove("in");
+        }
+    }
+})
+
+
